d5d72d37b38e1c948cc346d6e244f465e63107be
Brandon <98434819+Brandon2023@users.noreply.github.com>
2023-01-10 23:40:20 -0800

    teams rearranged to grid

--- bccatholics/pages/teams.php
+++ bccatholics/pages/teams.php
@@ -1,19 +1,87 @@
-<center>
-<font size="4"><b>Teams</b></font>
-<font color="gray" face="microsoft sans serif">
-<br />
-<br />
-Carney<br />
-HC<br />
-Imm<br />
-LFA<br />
-ND<br />
-SJB<br />
-St. Andrews<br />
-St. Anns<br />
-St. Pats<br />
-STA<br />
-STM<br />
-VC<br />
-</font>
-</center>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <section class="teams-grid">
+                <style> 
+                .teams-grid {
+                    display: grid;
+                    grid-template-columns: repeat(2, 1fr);
+                    padding: 30px 20px;
+                    grid-column-gap: 20px;
+                    grid-row-gap: 30px;
+                }
+
+                .container {
+                    position: relative;
+                    text-align: center;
+                }
+                </style>
+
+                <div class="teams-ww">
+                    <img src="americano.jpg" alt="americano">
+                    <h3>Carney</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="cappuccino.jpg" alt="cappuccino">
+                    <h3>HC</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="latte.jpg" alt="latte">
+                    <h3>Imm</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>LFA</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>ND</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>SJB</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>St. Andrews</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>St. Anns</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>St. Pats</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>STA</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="macchiato.jpg" alt="macchiato">
+                    <h3>STM</h3>
+                </div> 
+
+                <div class="teams-ww">
+                    <img src="eee.png" alt="macchiato">
+                    <h3>VC</h3>
+                </div> 
+            </section>
+        </div>
+    </body>
+</html>
