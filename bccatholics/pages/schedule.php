bfba8e8ba9d86f0067bde78a46813bf4cd10f52b
Brandon <98434819+Brandon2023@users.noreply.github.com>
2023-01-11 15:31:04 -0800

    Update schedule.php

--- bccatholics/pages/schedule.php
+++ bccatholics/pages/schedule.php
@@ -1,6 +1,6 @@
 <p align="center">* Games 15 & 17 played at Surrey Christian Middle School <br>* Ganes 27, 28, 29, & 30 played at Langley Events Center</p>
-<table width="100%">
-<tr><th><u>Game</u></th><th><u>Day</u></th><th><u>Time</u></th><th><u>Home Team</u></th><th><u>Away Team</u></th><th>&nbsp;</th></tr>
+<table>
+<div id="teare"><tr><th><u>Game</u></th><th><u>Day</u></th><th><u>Time</u></th><th><u>Home Team</u></th><th><u>Away Team</u></th><th>&nbsp;</th></tr></div>
 <?php
 
 include "schedule_config.php";
@@ -23,3 +23,19 @@ for($x=1;$x<$games;$x++) {
 ?>
 </table>
 <p>&nbsp;</p>
+
+
+<style>
+	td,th{
+		color:#FFF;
+		border:1px solid #ffffff;
+		padding:0.5%;
+	}
+
+
+	table{
+		width:120%;
+		text-align:center;
+		margin-left:-10.2%;
+	}
+</style>
